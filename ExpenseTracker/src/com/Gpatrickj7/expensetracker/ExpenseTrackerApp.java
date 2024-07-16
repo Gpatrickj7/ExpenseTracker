0c943de57cdcfcd233d1f943f33540dcf590b629
Gpatrickj7 <gunnarpatrickj@gmail.com>
2024-07-15 23:34:11 -0500

    created the project in eclipse, converted it to maven to help with dependencies, and implemented java fx for the project. i am excited to learn a more modern GUI interface. off to sleep!

---
+++ ExpenseTracker/src/com/Gpatrickj7/expensetracker/ExpenseTrackerApp.java
@@ -0,0 +1,28 @@
+package com.Gpatrickj7.expensetracker;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ExpenseTrackerApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Welcome to Expense Tracker!");
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Expense Tracker");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
